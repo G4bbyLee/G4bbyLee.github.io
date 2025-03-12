af2dd4def2095a87c856b586a27ceeecfc3f3817
Gabby <0014074@student.isf.edu.hk>
2025-03-12 10:40:52 +0800

    This is my final website

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ My personal site.
 - [flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) - center content and create structure to the site
 
 - I used AI to help figure out how to flip an image when you click it to reveal something else: https://github.com/copilot/share/c87f10b6-09a4-8047-a101-ca41805e2066
+
+- I used this website to figure out how to create the sidebar: https://www.geeksforgeeks.org/create-a-fixed-sidebar-using-html-and-css/
--- script.js
+++ script.js
@@ -3,3 +3,32 @@ document.querySelectorAll('.flip-container').forEach(container => {
       this.classList.toggle('flipped');
     });
   });
+
+
+const track = document.querySelector('.carousel-track');
+const items = document.querySelectorAll('.carousel-item');
+let interval;
+
+function startCarousel() {
+interval = setInterval(() => {
+    const firstItem = track.firstElementChild;
+    track.appendChild(firstItem);
+    track.style.transition = 'none';
+    track.style.transform = 'translateX(0)';
+    setTimeout(() => {
+    track.style.transition = 'transform 0.5s ease-in-out';
+    track.style.transform = 'translateX(calc(-20% - 20px))'; /* Adjust for width and gap */
+    }, 50);
+}, 3000); // Change image every 3 seconds
+}
+
+function stopCarousel() {
+clearInterval(interval);
+}
+
+items.forEach(item => {
+item.addEventListener('mouseover', stopCarousel);
+item.addEventListener('mouseout', startCarousel);
+});
+
+startCarousel();
